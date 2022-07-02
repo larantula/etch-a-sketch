3f1401f1ca8cf07caf540b975f3c4b1a924894f4
Lori <loriwaltenbury@gmail.com>
2022-07-01 21:23:28 -0400

    ok so it does the thing, i havent made the prompt to set the grid, but i did get two buttons that set a 16x16 or 100x100 and the rest works.
    shipping it.

--- js.js
+++ js.js
@@ -1,12 +1,77 @@
 //declare container
 const container = document.querySelector("#container");
 //const newGame = document.querySelector(".newGame");
-const sizePrompt = document.querySelector(".sizePrompt")
+//const sizePrompt = document.querySelector(".sizePrompt")
 
 
 
-//create a 16x16 grid
-for (i = 0; i < (Math.pow(16, 2)); i++){
+
+//16 button
+const sixteen = document.querySelector(".sixteen");
+
+sixteen.addEventListener(`click`, (e)=>{
+
+let el = document.querySelector("#container");
+while (el.firstChild) el.removeChild (el.firstChild);
+
+gridAmount = 16;
+
+
+        for (i = 0; i < (gridAmount * gridAmount); i++){
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            document.getElementById("container").appendChild(grid);
+            grid.addEventListener(`mouseover`, function (e) {
+                grid.style.backgroundColor = "#AA0000";
+            });
+        }
+
+        document.documentElement.style.setProperty(
+            "--container-grid-template-columns", "16"
+        )
+
+
+}
+)
+
+
+
+//100 button
+const hundred = document.querySelector(".hundred");
+
+hundred.addEventListener(`click`, (e) => {
+
+    let el = document.querySelector("#container");
+    while (el.firstChild) el.removeChild (el.firstChild);
+
+    gridAmount = 5;
+    
+        for (i = 0; i < (gridAmount * gridAmount); i++){
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            document.getElementById("container").appendChild(grid);
+            grid.addEventListener(`mouseover`, (e) => {
+                grid.style.backgroundColor = "#AA0000";
+            });
+        }
+
+        document.documentElement.style.setProperty(
+            "--container-grid-template-columns", "5"
+        )
+
+            console.log(gridAmount);
+    })
+
+    /*
+//clear button
+const clear = document.querySelector(".clear");
+    clear.addEventListener(`click`, window.location.reload());
+*/
+
+
+/*
+//create a grid
+for (i = 0; i < (gridAmount * gridAmount); i++){
     let grid = document.createElement("div");
     grid.classList.add("grid");
     document.getElementById("container").appendChild(grid);
@@ -14,6 +79,7 @@ for (i = 0; i < (Math.pow(16, 2)); i++){
         grid.style.backgroundColor = "#AA0000";
     });
 }
+*/
 
 /*
 sizePrompt.addEventListener(`click`, (e) => {
